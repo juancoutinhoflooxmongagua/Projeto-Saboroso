dd034e2802d65f138313d91c4302821598971591
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-03-18 12:35:36 -0300

    (ATOMIC COMMIT) feat: add refactor of code with include - all pages cut

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,38 @@
-var conn = require('./../inc/db');
+var conn = require('../inc/db');
+
+var menus = require('./../inc/menus');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  conn.query(`SELECT * FROM tb_menus ORDER BY title`, (err, results) => {
-    if (err) {
-      console.error("Erro na consulta SQL:", err);
-      return next(err); 
-    }
 
+  menus.getMenus().then(results => {
     res.render('index', { 
-      title: 'Restaurante Saboroso', 
-      menus: results || []
+      title: 'Sensational Restaurant!', 
+      menus: results,
+      isHome: true,
+      background: 'images/img_bg_1.jpg' // Adicionei o background aqui
     });
   });
+
 });
+
 
 router.get('/contacts', function(req, res, next){
   res.render('contact', { title: "Contato" }); 
 });
 
-router.get('/menus', function(req, res, next){
-  res.render('menu', { title: "Cardápio" }); 
+router.get('/menus', function(req, res, next) {
+
+  menus.getMenus().then(results => {
+    res.render('menus', { 
+      title: 'Our Menus', 
+      background: 'images/img_bg_1.jpg',
+      h1: 'saboreie nosso main menu',
+      menus: results
+    });
+  });
 });
 
 router.get('/reservations', function(req, res, next){
@@ -30,7 +40,11 @@ router.get('/reservations', function(req, res, next){
 });
 
 router.get('/services', function(req, res, next){
-  res.render('services', { title: "Nossos Serviços" }); 
+  res.render('services', {
+    title: 'Our Services',
+     background: 'images/img_bg_1.jpg',
+    h1: 'q prazer obg'
+  });
 });
 
 module.exports = router;
