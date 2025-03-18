30f6d9b9278d8c0ff45868615d7d0c9eb262f232
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-03-18 10:56:39 -0300

    fix: bugs in project

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,36 @@
-var conn = require('./../inc/db')
+var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title 
-  `, (err, results) => {
+  conn.query(`SELECT * FROM tb_menus ORDER BY title`, (err, results) => {
     if (err) {
-      console.log(err)
+      console.error("Erro na consulta SQL:", err);
+      return next(err); // Passa o erro para o middleware de erro
     }
 
     res.render('index', { 
       title: 'Restaurante Saboroso', 
-      menus: results
+      menus: results || [] // Garante que `menus` seja um array, evitando erro no template
     });
-  })
-  
+  });
 });
 
-router.get('/contacts', function(req, res,next){
-  res.render('contact')
-})
+router.get('/contacts', function(req, res, next){
+  res.render('contact', { title: "Contato" }); 
+});
 
+router.get('/menus', function(req, res, next){
+  res.render('menu', { title: "Cardápio" }); 
+});
 
-router.get('/menus', function(req, res,next){
-  res.render('menu')
-})
+router.get('/reservations', function(req, res, next){
+  res.render('reservation', { title: "Reservas" }); 
+});
 
-router.get('/reservations', function(req, res,next){
-  res.render('reservation')
-})
+router.get('/services', function(req, res, next){
+  res.render('services', { title: "Nossos Serviços" }); 
+});
 
-router.get('/services', function(req, res,next){
-  res.render('services')
-})
 module.exports = router;
