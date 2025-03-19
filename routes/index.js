5582d6d6c13b5a6565fb9751cff8db49550c2ebb
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-03-19 09:53:56 -0300

    feat: add method reservations

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var conn = require('./../inc/db');
 var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations')
 var express = require('express');
 var router = express.Router();
 
@@ -34,9 +35,36 @@ router.get('/menus', function(req, res, next) {
   });
 });
 
-router.get('/reservations', function(req, res, next){
-  res.render('reservation', { title: "Reservas" }); 
+router.get('/reservations', function(req, res, next) {
+  reservations.render(req, res);
 });
+
+router.post('/reservations', function(req, res, next) {
+
+  if(!req.body.name){
+    reservations.render(req, res, "Digite o nome");
+  }else if(!req.body.email){
+    reservations.render(req, res, "Digite o email");
+  }else if(!req.body.people){
+    reservations.render(req, res, "Selecione a quantidade numero de pessoas");
+  }else if(!req.body.date){
+    reservations.render(req, res, "Selecione a data");
+  }else if(!req.body.time){
+    reservations.render(req, res, "Selecione a hora");
+  }else{
+     reservations.save(req.body).then(results => {
+      req.body = {};
+      io.emit('dashboard update');
+
+      reservations.render(req, res, null, "Reserva realizada com sucesso!");
+
+    }).catch(err => {
+      reservations.render(req, res, err.message);
+    });
+  }
+
+});
+
 
 router.get('/services', function(req, res, next){
   res.render('services', {
